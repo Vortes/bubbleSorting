f381772371ef3f2409eac6ea036e1f739e05c1ba
Vortes <zeyu.weng@gmail.com>
2020-01-12 15:31:54 -0500

    figuring out sorting capabilities

--- visualBubbleSort/sort.js
+++ visualBubbleSort/sort.js
@@ -1,5 +1,5 @@
 var div_arry = []
-
+var div_height = 0
 
 function BubbleSort(arry) {
     var swap;
@@ -34,8 +34,8 @@ function randomIntFromInterval(min, max) {
 function setGraph() {
     for(i = 0; i < 43; i++){
         var div = document.createElement('div');
-        div_arry.push(div)
         div.setAttribute("id", "Div" + i.toString());
+        div_arry.push(div)
         var height = randomIntFromInterval(10, 300)
         div.style.width = "15px";
         div.style.height = height.toString() + "px";
@@ -43,12 +43,18 @@ function setGraph() {
         div.style.margin = "0.5%";
         div.style.float = 'left'
         document.getElementById("main").appendChild(div);
-        var x = div.offsetLeft
-        var y = div.offsetTop;
-        style = window.getComputedStyle(div)
-        div_height = style.getPropertyValue('height')
-        console.log(div_height)
-        console.log(x,y)
+    }
+}
+
+
+function sort(){
+    // change the pos of div id 1 and div id 2
+    for(i = 0; i < 43; i++){
+        console.log("Div" + i.toString())
+        var element = document.getElementById["Div1"]
+        console.log(element)
+        element.style.left = '0px'
+        element.style.top = '0px'
     }
 }
 
